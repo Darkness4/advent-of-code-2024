77a3b35b838ffb940ef15c94357832c7f7d059bd
Nguyen Marc <nguyen_marc@live.fr>
2024-12-01 14:57:56 +0100

    feat(day1): naive

--- src/day1.zig
+++ src/day1.zig
@@ -1,33 +1,68 @@
 const std = @import("std");
 
-var input = @embedFile("day1.txt");
-var input_test1 = @embedFile("day1p1_test.txt");
-var input_test2 = @embedFile("day1p2_test.txt");
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
 
-fn day1(data: []const u8) !i64 {
+var input = @embedFile("day1.txt");
+var input_test = @embedFile("day1_test.txt");
+
+fn day1(data: []const u8) !u64 {
     var lines = std.mem.splitSequence(u8, data, "\n");
 
+    var list_a = try std.ArrayList(i64).initCapacity(allocator, 1000);
+    defer list_a.deinit();
+
+    var list_b = try std.ArrayList(i64).initCapacity(allocator, 1000);
+    defer list_b.deinit();
+
+    // Read two columns of numbers
     while (lines.next()) |line| {
         if (line.len == 0) {
             continue;
         }
-        std.debug.print("{s}\n", .{line});
+        var groups = std.mem.tokenizeAny(u8, line, " ");
+        try list_a.append(try std.fmt.parseInt(i64, groups.next().?, 10));
+        try list_b.append(try std.fmt.parseInt(i64, groups.next().?, 10));
     }
 
-    return 0;
+    // Sort the lists
+    std.mem.sort(i64, list_a.items, {}, comptime std.sort.asc(i64));
+    std.mem.sort(i64, list_b.items, {}, comptime std.sort.asc(i64));
+
+    // Compute the distance
+    var acc: u64 = 0;
+    for (list_a.items, list_b.items) |a, b| {
+        acc += @abs(a - b);
+    }
+
+    return acc;
 }
 
 fn day1p2(data: []const u8) !i64 {
     var lines = std.mem.splitSequence(u8, data, "\n");
 
+    var occurences_a: [100000]i64 = [_]i64{0} ** 100000;
+    var occurences_b: [100000]i64 = [_]i64{0} ** 100000;
+
+    // Read two columns of numbers
     while (lines.next()) |line| {
         if (line.len == 0) {
             continue;
         }
-        std.debug.print("{s}\n", .{line});
+        var groups = std.mem.tokenizeAny(u8, line, " ");
+        const a = try std.fmt.parseInt(usize, groups.next().?, 10);
+        occurences_a[a] += 1;
+        const b = try std.fmt.parseInt(usize, groups.next().?, 10);
+        occurences_b[b] += 1;
     }
 
-    return 0;
+    // Compute similarity
+    var acc: i64 = 0;
+    for (0.., occurences_a, occurences_b) |i, a, b| {
+        acc += @as(i64, @intCast(i)) * a * b;
+    }
+
+    return acc;
 }
 
 pub fn main() !void {
@@ -41,11 +76,11 @@ pub fn main() !void {
 }
 
 test "day1" {
-    const result = try day1(input_test1);
-    try std.testing.expect(result == 0);
+    const result = try day1(input_test);
+    try std.testing.expect(result == 11);
 }
 
 test "day1p2" {
-    const result = try day1p2(input_test2);
-    try std.testing.expect(result == 0);
+    const result = try day1p2(input_test);
+    try std.testing.expect(result == 31);
 }
