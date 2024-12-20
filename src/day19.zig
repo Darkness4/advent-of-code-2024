2ee52f885d1d5efa7152c59e267b963e03b87909
Nguyen Marc <nguyen_marc@live.fr>
2024-12-20 11:05:31 +0100

    chore(day19): add break

--- src/day19.zig
+++ src/day19.zig
@@ -21,16 +21,17 @@ fn is_possible(
         return true;
     }
 
-    var total: bool = false;
+    var res = false;
     for (patterns) |pattern| {
         if (std.mem.startsWith(u8, elem, pattern)) {
             const remaining = elem[pattern.len..];
-            total = total or try is_possible(allocator, cache, patterns, remaining);
+            res = res or try is_possible(allocator, cache, patterns, remaining);
+            if (res) break;
         }
     }
 
-    try cache.put(elem, total);
-    return total;
+    try cache.put(elem, res);
+    return res;
 }
 
 fn day19(allocator: std.mem.Allocator, data: []const u8) !usize {
@@ -59,20 +60,72 @@ fn day19(allocator: std.mem.Allocator, data: []const u8) !usize {
     return acc;
 }
 
-fn day19p2(_: []const u8) !usize {
-    return 0;
+fn count(
+    allocator: std.mem.Allocator,
+    cache: *std.StringHashMap(usize),
+    patterns: [][]const u8,
+    elem: []const u8,
+) !usize {
+    if (cache.get(elem)) |value| {
+        return value;
+    }
+
+    // Elem has every fragment.
+    if (elem.len == 0) {
+        try cache.put(elem, 1);
+        return 1;
+    }
+
+    var total: usize = 0;
+    for (patterns) |pattern| {
+        if (std.mem.startsWith(u8, elem, pattern)) {
+            const remaining = elem[pattern.len..];
+            total += try count(allocator, cache, patterns, remaining);
+        }
+    }
+
+    try cache.put(elem, total);
+    return total;
+}
+
+fn day19p2(allocator: std.mem.Allocator, data: []const u8) !usize {
+    var lines = std.mem.splitScalar(u8, data, '\n');
+
+    const pattern_line = lines.next() orelse unreachable;
+    var patterns_it = std.mem.splitSequence(u8, pattern_line, ", ");
+
+    var patterns_list = std.BoundedArray([]const u8, 500){};
+    while (patterns_it.next()) |line| {
+        patterns_list.appendAssumeCapacity(line);
+    }
+    const patterns = patterns_list.slice();
+
+    var cache = std.StringHashMap(usize).init(allocator);
+    defer cache.deinit();
+
+    _ = lines.next() orelse unreachable;
+
+    var acc: usize = 0;
+    while (lines.next()) |line| {
+        const c = try count(allocator, &cache, patterns, line);
+        acc += c;
+    }
+
+    return acc;
 }
 
 pub fn main() !void {
     var timer = try std.time.Timer.start();
     const result_p1 = try day19(std.heap.page_allocator, input);
     const p1_time = timer.lap();
-    const result_p2 = try day19p2(input);
+    const result_p2 = try day19p2(std.heap.page_allocator, input);
     const p2_time = timer.read();
     std.debug.print("day19 p1: {} in {}ns\n", .{ result_p1, p1_time });
     std.debug.print("day19 p2: {} in {}ns\n", .{ result_p2, p2_time });
 
-    var bench = zbench.Benchmark.init(std.heap.page_allocator, .{});
+    var bench = zbench.Benchmark.init(std.heap.page_allocator, .{
+        .track_allocations = true,
+    });
     defer bench.deinit();
     try bench.add("day19 p1", struct {
         pub fn call(allocator: std.mem.Allocator) void {
@@ -80,8 +133,8 @@ pub fn main() !void {
         }
     }.call, .{});
     try bench.add("day19 p2", struct {
-        pub fn call(_: std.mem.Allocator) void {
-            _ = day19p2(input) catch unreachable;
+        pub fn call(allocator: std.mem.Allocator) void {
+            _ = day19p2(allocator, input) catch unreachable;
         }
     }.call, .{});
     try bench.run(std.io.getStdOut().writer());
@@ -97,8 +150,8 @@ test "day19" {
 }
 
 test "day19p2" {
-    const result = try day19p2(input_test);
-    const expect = 0;
+    const result = try day19p2(std.heap.page_allocator, input_test);
+    const expect = 16;
     std.testing.expect(result == expect) catch |err| {
         std.debug.print("got: {}, expect: {}\n", .{ result, expect });
         return err;
